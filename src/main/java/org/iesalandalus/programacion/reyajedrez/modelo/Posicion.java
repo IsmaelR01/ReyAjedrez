6bb6f61ac50210bb19d58722f8ecc8938c1050e2
ismael <ismaelrivasn@gmail.com>
2023-11-13 14:35:26 +0100

    Hacemos los getters y setters comprobando su excepciones

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
@@ -4,4 +4,25 @@ public class Posicion {
     private int fila;
     private char columna;
 
+    public int getFila() {
+        return fila;
+    }
+
+    public void setFila(int fila) {
+        if(fila<1 || fila > 8) {
+            throw new IllegalArgumentException("ERROR:La fila introducida no es correcta");
+        }
+        this.fila = fila;
+    }
+
+    public char getColumna() {
+        return columna;
+    }
+
+    public void setColumna(char columna) {
+        if(columna <'a' || columna >'h') {
+            throw new IllegalArgumentException("ERROR:La columna introducida no es correcta");
+        }
+        this.columna = columna;
+    }
 }
