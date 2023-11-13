7fb72107400b287f181a9bf0391da3556aa9aa29
ismael <ismaelrivasn@gmail.com>
2023-11-13 14:18:25 +0100

    Creando la clase enumerado Color

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Direccion.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Direccion.java
@@ -1,10 +1,11 @@
 package org.iesalandalus.programacion.reyajedrez.modelo;
 
 public enum Direccion {
-    NORTE, NORESTE, ESTE, SURESTE, SUR, SUROESTE, OESTE, NOROESTE, ENROQUE_CORTO,ENROQUE_LARGO;
+    NORTE("Norte"), NORESTE("Noreste"), ESTE("Este"), SURESTE("Sureste"), SUR("Sur"), SUROESTE("Suroeste"), OESTE("Oeste"), NOROESTE("Noroeste"), ENROQUE_CORTO("Enroque corto"),ENROQUE_LARGO("Enroque largo");
 
-    private String cadenaAMostrar;
-    private void Direccion(String cadenaAMostrar) {
+    private  String cadenaAMostrar;
+    private  Direccion(String cadenaAMostrar) {
+
         this.cadenaAMostrar = cadenaAMostrar;
     }
 
