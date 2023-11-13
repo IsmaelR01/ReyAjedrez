b9117d192fd42c1e995499b97f4b375226e6a394
ismael <ismaelrivasn@gmail.com>
2023-11-13 14:42:44 +0100

    he creado el constructor Posicion

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
@@ -4,12 +4,18 @@ public class Posicion {
     private int fila;
     private char columna;
 
+    public Posicion(int fila, char columna) {
+        setFila(fila);
+        setColumna(columna);
+    }
+
+
     public int getFila() {
         return fila;
     }
 
     public void setFila(int fila) {
-        if(fila<1 || fila > 8) {
+        if(fila < 1 || fila > 8) {
             throw new IllegalArgumentException("ERROR:La fila introducida no es correcta");
         }
         this.fila = fila;
@@ -25,4 +31,5 @@ public class Posicion {
         }
         this.columna = columna;
     }
+
 }
